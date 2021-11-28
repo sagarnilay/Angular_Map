a5b7e2a123ca6a6d59508ca4b390d156f2f8ea49
sagarnilay <58388134+sagarnilay@users.noreply.github.com>
2021-11-28 22:49:31 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+declare var google: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'map';
+export class AppComponent implements AfterViewInit {
+  map: any;
+  @ViewChild('map') mapElement: any;
+  lat = 43.879078;
+  lng = -103.4615581;
+  markers = [
+    { lat: 18.9342195, lng: 72.8256279 },
+    { lat: 18.9369012, lng: 72.8253435 },
+    { lat: 18.9203886, lng: 72.8301305999999}
+  ];
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    const mapProperties = {
+      center: new google.maps.LatLng(this.lat, this.lng),
+      zoom: 2,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapProperties);
+
+    this.markers.forEach(location => {
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(location.lat, location.lng),
+        map: this.map
+      });
+    });
+  }
 }
